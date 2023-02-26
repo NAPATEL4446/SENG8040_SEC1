656793bd30a67c3e8c8213c910847e1eacb9631a
NAPATEL4446 <126364986+NAPATEL4446@users.noreply.github.com>
2023-02-25 19:52:19 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitLab;
+using NUnit;
+
+namespace TriangleTestbynidhi;
+
+public class TriangleTest
+{
+    [TestFixture]
+    public class TriangleTest1
+
+    {
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The Triangle is valid.";
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
---
+++ Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+
+namespace Nunitlab
+{
+    public static class Triangle
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The Triangle is valid.";
+            }
+            else
+            {
+                result = "The Triangle is not valid.";
+            }
+            return result;
+        }
+    }
+}
